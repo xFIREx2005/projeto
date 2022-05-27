f3e16bc1c53801d64314bdfede7606ac67da29d0
xFIREx2005 <78804981+xFIREx2005@users.noreply.github.com>
2022-05-27 11:28:59 -0300

    Initial commit

---
+++ app/src/main/java/com/example/projeto/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.projeto;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void play(View view){
+        Intent it = new Intent(this, MainActivity2.class);
+        startActivity(it);
+    }
+}
