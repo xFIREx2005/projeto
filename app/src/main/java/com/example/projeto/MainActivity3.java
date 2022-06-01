4352e8fa5e2160d48fd04d22f036fd4976f35247
xFIREx2005 <kaique.santos150@etec.sp.gov.br>
2022-05-31 22:30:34 -0300

    31/05.v2
    legal e bonito

--- app/src/main/java/com/example/projeto/MainActivity3.java
+++ app/src/main/java/com/example/projeto/MainActivity3.java
@@ -2,19 +2,32 @@ package com.example.projeto;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity3 extends AppCompatActivity {
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+    }
+    public int numTxt = 0;
+    public void ChooseTxt(View view){
         TextView txt = (TextView) findViewById(R.id.txthis);
-        txt.setText("@string/his_fa_1");
+        String his_fa1_1 = getString(R.string.his_na1);
+        numTxt++;
+        if(numTxt == 1)
+        {
+            txt.setText(his_fa1_1);
+        }
+        if(numTxt >= 2)
+        {
+            Intent it = new Intent(this, MainActivity4.class);
+            startActivity(it);
+        }
     }
 
 
